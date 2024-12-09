a9082efb290aac004db7f9d0fd1259c46c60ed64
jgraessle <j_graessle@mail.harpercollege.edu>
2024-12-08 19:57:43 -0800

    New Calculator Class
    The Calculator class was added that has 8 methods that find the sum, average value, maximum value, and minimum value of an array using streams or for loops. Tests were also added to ensure the functionality of these methods.

--- src/main/java/com/csc/Calculator.java
+++ src/main/java/com/csc/Calculator.java
@@ -1,6 +1,78 @@
 package com.csc;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Calculator {
+
+  public int minWithFor(int[] array) {
+    int min = 0;
+    for (int i = 0; i < array.length; i++ ) {
+      if (i == 0) {
+        min = array[i];
+      }
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
+
+  public int maxWithFor(int[] array) {
+    int max = 0;
+    for (int i = 0; i < array.length; i++ ) {
+      if (i == 0) {
+        max = array[i];
+      }
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
+
+  public int avgWithFor(int[] array) {
+    int avg = 0;
+    for (int i = 0; i < array.length; i++) {
+      avg = avg + array[i];
+    }
+    avg = avg / array.length;
+    return avg;
+  }
+
+  public int sumWithFor(int[] array) {
+    int sum = 0;
+    for (int i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    return sum;
+  }
+
+  public int minWithStream(int[] array) {
+    Stream.of(array);
+    int min = Arrays.stream(array).min().getAsInt();
+    return min;
+  }
+
+  public int maxWithStream(int[] array) {
+    Stream.of(array);
+    int max = Arrays.stream(array).max().getAsInt();
+    return max;
+  }
+
+  public int avgWithStream(int [] array) {
+    Stream.of(array);
+    double avg = Arrays.stream(array).average().getAsDouble();
+    int avg1 = (int)avg;
+    return avg1;
+  }
+
+  public int sumWithStream(int [] array) {
+    Stream.of(array);
+    int sum = Arrays.stream(array).sum();
+    return sum;
+  }
+
   public static void main(String[] args) {
     System.out.println("This class can house your driver program and / or your functions.");
   }
--- src/test/java/com/csc/TestCalculator.java
+++ src/test/java/com/csc/TestCalculator.java
@@ -1,21 +1,47 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestCalculator {
 
-  Calculator calculator;
-
-  @BeforeEach
-  void setUp() {
-    calculator = new Calculator();
-  }
+  Calculator calculator = new Calculator();
+  int[] testArray = {1, 4, 7, 2, 5, 8, 3, 6};
 
   @Test
   void itWorks() {
     assertEquals(true, true);
+  }
+  @Test
+  void minForWorks() {
+    assertEquals(1, calculator.minWithFor(testArray));
+  }
+  @Test
+  void maxForWorks() {
+    assertEquals(8, calculator.maxWithFor(testArray));
+  }
+  @Test
+  void avgForWorks() {
+    assertEquals(4, calculator.avgWithFor(testArray));
+  }
+  @Test
+  void sumForWorks() {
+    assertEquals(36, calculator.sumWithFor(testArray));
+  }
+  @Test
+  void minStreamWorks() {
+    assertEquals(1, calculator.minWithStream(testArray));
+  }
+  @Test
+  void maxStreamWorks() {
+    assertEquals(8, calculator.maxWithStream(testArray));
+  }
+  @Test
+  void avgStreamWorks() {
+    assertEquals(4, calculator.avgWithStream(testArray));
+  }
+  @Test
+  void sumStreamWorks() {
+    assertEquals(36, calculator.sumWithStream(testArray));
   }
 }
